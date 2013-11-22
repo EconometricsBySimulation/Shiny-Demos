1a12a6f27efbbeb7e44e17f1592be5272d2fb768
Francis Smart <francis.smart@gmail.com>
2013-11-22 14:23:02 -0500

    Update ui.R

--- Survey/ui.R
+++ Survey/ui.R
@@ -4,26 +4,33 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   #  Application title
-  headerPanel("Shiny-Survey Tool v.01"),
+  headerPanel("Shiny Survey Tool v.01"),
   
   sidebarPanel(
+      # This is intentionally an empty object.
       h6(textOutput("save.results")),
       h5("Created by:"),
             tags$a("Econometrics by Simulation", 
                    href="http://www.econometricsbysimulation.com"),
             h5("For details on how data is generated:"),
             tags$a("Blog Post", 
-                   href="http://www.econometricsbysimulation.com/2013/11/a-shiny-app-for-playing-with-ols.html"),
+                   href=paste0("http://www.econometricsbysimulation.com/",
+                               "2013/19/Shiny-Survey-Tool.html")),
             h5("Github Repository:"),
-            tags$a("OLS-Demo-App", 
-                   href="https://github.com/EconometricsBySimulation/OLS-demo-App/"),
+            tags$a("Survey-Tool", 
+                   href=paste0("https://github.com/EconometricsBySimulation/",
+                   "Shiny-Demos/tree/master/Survey")),
+            # Display the page counter text.
             h5(textOutput("counter"))
       ),
 
   
   # Show a table summarizing the values entered
   mainPanel(
+    # Main Action is where most everything is happenning in the
+    # object (where the welcome message, survey, and results appear)
     uiOutput("MainAction"),
+    # This displays the action putton Next.
     actionButton("Click.Counter", "Next")    
     )
 ))
