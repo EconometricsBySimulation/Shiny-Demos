26ff726fdc56e299179529ac065b0853ee2b9833
Francis Smart <francis.smart@gmail.com>
2014-03-08 02:01:47 +0200

    Update ui.R

--- Function-Counter/ui.R
+++ Function-Counter/ui.R
@@ -10,7 +10,7 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
     
     sliderInput("FS", "Display Range", 
-                min=1, max=53478, value=c(1,50)),
+                min=1, max=5000, value=c(1,50)),
     
     sliderInput("ndisplay", "# of Functions to Display", 
                 min=0, max=200, value=50),
@@ -20,7 +20,7 @@ shinyUI(pageWithSidebar(
     textInput("tsearch", "Search for function", "list"),
     tableOutput("tresult"),
           
-    h5("Thank you to John Myles White for compiling a list of word frequencies (in 2009) from source files for all CRAN Packages:"),
+    h6('Thank you to John Myles White for compiling a list of word frequencies (in 2009) from source files for all CRAN Packages.'),
     tags$a("John Myles White", 
            href="http://www.johnmyleswhite.com/notebook/2009/12/07/r-function-usage-frequencies/"),
     h5("Created by:"),
@@ -32,7 +32,8 @@ shinyUI(pageWithSidebar(
     h5("Github Repository:"),
     tags$a("Function Counter", 
            href="https://github.com/EconometricsBySimulation/Shiny-Demos/blob/master/Function-Counter/"),
-    h5(textOutput("counter"))
+    h5(textOutput("counter")),
+    HTML('<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Feconometricsbysimulation.shinyapps.io%2FFCount%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>')
     ),
   
   # Show a table summarizing the values entered
